bbf03192c594907088cdf54d2be5101541754cb0
jaswanth2222 <ljaswanth222@gmail.com>
2023-08-08 18:26:44 +0530

    adding styles to nav bar in Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ const Header = () => {
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvCrtmEN9vahUD5tHWmx8MXdQycacsG0bfNg&usqp=CAU" alt="" className="w-24" />
 
             <div className=" flex list-none me-10 ">
-                <Link to = "/home"><li className="mx-2 hover:cursor-pointer">Home</li> </Link>
-                <Link to="/about"><li className="mx-2 hover:cursor-pointer">About</li> </Link>
+                <Link to = "/home"><li className="mx-2 hover:cursor-pointer hover:underline hover:underline-offset-4 hover:decoration-orange-500 hover:decoration-2 hover:decoration-solid">Home</li> </Link>
+                <Link to="/about"><li className="mx-2 hover:cursor-pointer hover:underline hover:underline-offset-4 hover:decoration-orange-500 hover:decoration-2 hover:decoration-solid">About</li> </Link>
                 <Link to = "/contact">
-                    <li className="mx-2 hover:cursor-pointer">Contact</li>
+                    <li className="mx-2 hover:cursor-pointer hover:underline hover:underline-offset-4 hover:decoration-orange-500 hover:decoration-2 hover:decoration-solid">Contact</li>
                 </Link>
             </div>
                 
